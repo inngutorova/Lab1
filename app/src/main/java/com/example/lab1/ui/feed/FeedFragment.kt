d26369ee217c7599f1769c406d843447f4d1bb94
inngutorova <inngutorova@gmail.com>
2025-12-20 23:09:36 +0300

    реализовано получение данных из апи и базовый интерфейс сообщения

--- app/src/main/java/com/example/lab1/ui/feed/FeedFragment.kt
+++ app/src/main/java/com/example/lab1/ui/feed/FeedFragment.kt
@@ -1,44 +1,76 @@
-package com.example.messenger.ui.feed
+package com.example.lab1.ui.feed
 
 import android.os.Bundle
 import android.util.Log
 import android.view.*
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.lifecycleScope
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import android.widget.Button
+
 import com.example.lab1.R
+import kotlinx.coroutines.flow.collectLatest
 
 class FeedFragment : Fragment() {
+
+    private val viewModel: FeedViewModel by viewModels()
+    private val adapter = MessageAdapter()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         Log.i("Lifecycle", "FeedFragment onCreate")
     }
 
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?) =
-        inflater.inflate(R.layout.fragment_feed, container, false)
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        val view = inflater.inflate(R.layout.fragment_feed, container, false)
 
+        val recyclerView = view.findViewById<RecyclerView>(R.id.recyclerView)
+        val refreshButton = view.findViewById<Button>(R.id.btnRefresh)
+
+        recyclerView.layoutManager = LinearLayoutManager(requireContext())
+        recyclerView.adapter = adapter
+
+        viewLifecycleOwner.lifecycleScope.launchWhenStarted {
+            viewModel.messages.collectLatest { messages ->
+                adapter.submitList(messages)
+            }
+        }
+
+        refreshButton.setOnClickListener {
+            viewModel.refresh()
+        }
+
+        return view
+    }
 
     override fun onStart() {
-        super.onStart();
+        super.onStart()
         Log.i("Lifecycle", "FeedFragment onStart")
     }
 
     override fun onResume() {
-        super.onResume();
+        super.onResume()
         Log.i("Lifecycle", "FeedFragment onResume")
     }
 
     override fun onPause() {
-        super.onPause();
+        super.onPause()
         Log.i("Lifecycle", "FeedFragment onPause")
     }
 
     override fun onStop() {
-        super.onStop();
+        super.onStop()
         Log.i("Lifecycle", "FeedFragment onStop")
     }
 
     override fun onDestroy() {
-        super.onDestroy();
+        super.onDestroy()
         Log.i("Lifecycle", "FeedFragment onDestroy")
     }
 }
